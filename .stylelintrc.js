80e0f2eb63ce9cccaf9e7170974ff2967a1353c0
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-09-04 10:28:55 +0800

    copy

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
-  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties', 'stylelint-scss'],
   rules: {
     'plugin/declaration-block-no-ignored-properties': true,
     'comment-empty-line-before': null,
@@ -8,7 +8,11 @@ module.exports = {
     'function-name-case': 'lower',
     'no-descending-specificity': null,
     'no-invalid-double-slash-comments': null,
-    'rule-empty-line-before': 'always',
+    'block-no-empty': null,
+    'value-keyword-case': null,
+    'rule-empty-line-before': ['always', { except: ['after-single-line-comment', 'first-nested'] }],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
-  ignoreFiles: ['node_modules/**/*', 'build/**/*'],
+  ignoreFiles: ['node_modules/**/*', 'build/**/*', 'dist/**/*'],
 };
